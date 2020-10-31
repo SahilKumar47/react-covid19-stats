aec6e1c1dce14326db4d7a96d836f71df18f9292
Sahil Kumar <49636096+SahilKumar47@users.noreply.github.com>
2020-10-31 21:11:10 +0530

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -5,7 +5,7 @@ const API_ENDPOINTS = {
     "https://api.covid19india.org/v2/state_district_wise.json",
   TRAVEL_HISTORY: "https://api.covid19india.org/travel_history.json",
   RAW_DATA: "https://api.covid19india.org/raw_data.json",
-  STATES_DAILY_CHANGES: "https://api.covid19india.org/states_daily.json",
+  STATES_DAILY_CHANGES: "https://api.covid19india.org/states_daily.json", 
   DEATHS_AND_RECOVERIES: "https://api.covid19india.org/deaths_recoveries.json",
   ESSENTIALS_AND_RESOURCES:
     "https://api.covid19india.org/resources/resources.json"
